de75318dd41ce874f747945c631b4fef95d5fc07
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-07-12 20:05:47 -0300

    refactor: adding strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ToastContainer } from 'react-toastify';
@@ -7,11 +8,11 @@ import { GlobalStyles } from './styles/GlobalStyles';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  //<React.StrictMode>
-  <>
-    <App />
-    <GlobalStyles />
-    <ToastContainer />
-  </>
-  //</React.StrictMode>
+  <React.StrictMode>
+    <>
+      <App />
+      <GlobalStyles />
+      <ToastContainer />
+    </>
+  </React.StrictMode>
 );
